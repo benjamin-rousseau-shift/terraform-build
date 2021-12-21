ab100cc4774f66469bab6ea6c3c92d0a0dd42b24
heavenlyren <heavenlyren@gmail.com>
2021-12-21 13:52:34 +0000

    Automate PAN OS Configuration

--- panos-objects.tf
+++ panos-objects.tf
@@ -1,12 +1,12 @@
 # Address Objects
 resource "panos_address_object" "local_range" {
-  name        = "LOCAL_${var.enterprise}-${var.environment}-${var.region}-IP-RANGE_${var.IPAddressPrefix.0.0}"
+  name        = "LOCAL_${var.enterprise}-${var.environment}-${var.region}-IP-RANGE_${var.IPAddressPrefix}.0.0"
   value       = "${var.IPAddressPrefix}.0.0/16"
   description = ""
 }
 
 resource "panos_address_object" "local_mgmt" {
-  name        = "LOCAL_${var.enterprise}-${var.environment}-${var.region}-${var.project}1_${var.IPAddressPrefix.5.254}"
+  name        = "LOCAL_${var.enterprise}-${var.environment}-${var.region}-${var.project}1_${var.IPAddressPrefix}.5.254"
   value       = "${var.IPAddressPrefix}.0.254"
   description = ""
 }
