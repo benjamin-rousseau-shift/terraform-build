48f9568d81e3753734cd00ec3593d1621ee4b2ac
heavenlyren <heavenlyren@gmail.com>
2021-12-21 11:01:22 +0000

    Changed IKE Crypto

--- panos-crypto.tf
+++ panos-crypto.tf
@@ -1,8 +1,8 @@
-resource "panos_ike_crypto_profile" "example" {
-  name = "example"
-  dh_groups = ["group1", "group2"]
-  authentications = ["md5", "sha1"]
-  encryptions = ["des"]
-  lifetime_value = 8
-  authentication_multiple = 3
+resource "panos_ike_crypto_profile" "default" {
+  name = "AES-128-SHA256-G2-86400"
+  dh_groups = ["group2"]
+  authentications = ["SHA256"]
+  encryptions = ["aes-168-cbc"]
+  lifetime_value = 24
+  authentication_multiple = 0
 }
