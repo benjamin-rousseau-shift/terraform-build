fbeb987c96612396fbfa19aaf581bd334a8f3028
heavenlyren <heavenlyren@gmail.com>
2021-12-21 17:40:31 +0000

    Added DNS Rule

--- panos/panos-firewall-policy.tf
+++ panos/panos-firewall-policy.tf
@@ -42,6 +42,20 @@ resource "panos_security_policy_group" "default" {
   }
 
   rule {
+    name                  = "PERMIT DNS TO DOMAIN CONTROLLER LOCAL"
+    source_zones          = [panos_zone.internal.name, panos_zone.vpn_s2s.name]
+    source_addresses      = [var.domain_controller, panos_address_object.local_mgmt.name]
+    source_users          = ["any"]
+    hip_profiles          = ["any"]
+    destination_zones     = [panos_zone.internal.name, panos_zone.vpn_s2s.name]
+    destination_addresses = [var.domain_controller, panos_address_object.local_mgmt.name]
+    applications          = ["dns"]
+    services              = ["application-default"]
+    categories            = ["any"]
+    action                = "allow"
+  }
+
+  rule {
     name                  = "PERMIT VPN-S2S LOCAL"
     source_zones          = [panos_zone.untrust.name]
     source_addresses      = [var.ov_pa_pub, "${var.IPAddressPrefix}.1.254"]
