76935d172d12e0bce798da011109774d2fc5ee9c
heavenlyren <heavenlyren@gmail.com>
2021-12-22 16:49:10 +0000

    Changed variables to fit terraform cloud

--- cloud-instances/aks/variables.tf
+++ cloud-instances/aks/variables.tf
@@ -1,24 +1,3 @@
-variable "SHIFT_CLOUD_INSTANCE" {
-  type        = string
-  description = "The name of the cloud instance to build."
-}
-
-variable "SP_CLIENT_ID" {
-  type        = string
-  description = "The client id used for accessing the azure subscription."
-}
-
-variable "SP_CLIENT_SECRET" {
-  type        = string
-  description = "The client secret used for accessing the azure subscription."
-}
-
-
-variable "INSTANCE_CLOUD_RESOURCE_GROUP_NAME" {
-  type        = string
-  description = "The name of the instance of the resource group."
-}
-
 variable "AKS_WEB_VM_SIZE" {
   type        = string
   description = "The vm size used to populate the aks web."
@@ -31,12 +10,6 @@ variable "AKS_WEB_NODE_COUNT" {
   default     = 3
 }
 
-variable "AKS_WEB_SUBNET_ID" {
-  type        = string
-  description = "The id of the subnet for the aks web."
-}
-
-
 variable "AKS_WORKER_VM_SIZE" {
   type        = string
   description = "The vm size used to populate the aks web."
@@ -47,11 +20,6 @@ variable "AKS_WORKER_NODE_COUNT" {
   type        = number
   description = "The number of node of the AKS worker."
   default     = 3
-}
-
-variable "AKS_WORKER_SUBNET_ID" {
-  type        = string
-  description = "The id of the subnet for the aks worker."
 }
 
 variable "enterprise" {
