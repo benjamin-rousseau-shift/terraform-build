e5cb13e241f7f35461c08bd3f703ce887b2dac0b
heavenlyren <heavenlyren@gmail.com>
2022-02-25 15:18:06 +0000

    Updated providers version

--- panos/main.tf
+++ panos/main.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
+      version = "~>2.98.0"
     }
     panos   = {
       source  = "paloaltonetworks/panos"
-      version = "1.8.3"
+      version = "~>=1.10.0"
     }
   }
 }
